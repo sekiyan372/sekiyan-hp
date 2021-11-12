00362d79309344867489f268f5c27abf06c5f8eb
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2021-11-13 00:57:18 +0900

    tailwindcssを導入

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/components/**/*.tsx', './src/pages/**/*.tsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
