b0ccdc3a7cff85a111f6ef6eeb571dc6fae68c21
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2021-11-13 02:03:52 +0900

    sectionを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,35 @@ import Head from '~/components/Head'
 const Home: NextPage = () => {
   return (
     <div>
-      <Head title="トップページ" />
+      <Head />
 
-      <h1>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+      <section className="w-full h-screen">
+        <h1>Welcome to Sekiyan&#039;s Site</h1>
+      </section>
+
+      <section className="w-full h-screen">
+        <h2>profile</h2>
+        <div>
+          <p>name: Suiki Sekiya</p>
+          <p>from: Nagoya Aichi</p>
+        </div>
+      </section>
+
+      <section className="w-full h-screen">
+        <h2>history</h2>
+      </section>
+
+      <section className="w-full h-screen">
+        <h2>product</h2>
+      </section>
+
+      <section className="w-full h-screen">
+        <h2>hobby</h2>
+      </section>
+
+      <section className="w-full h-screen">
+        <h2>contact</h2>
+      </section>
     </div>
   )
 }
