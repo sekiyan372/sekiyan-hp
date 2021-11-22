6fe884e91993eee43646619cb260df0fb2b16706
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2021-11-23 03:39:13 +0900

    リンクを作成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
 import { VFC } from 'react'
+import Link from 'next/link'
 
 const Header: VFC = () => {
   return(
-    <header className="overflow-hidden p-3">
-      <span className="text-3xl">Sekiyan372</span>
+    <header className="overflow-hidden p-3 flex justify-between">
+      <Link href="/" passHref>
+        <div className="text-3xl cursor-pointer">Sekiyan372</div>
+      </Link>
+      <Link href="/product" passHref>
+        <div className="text-2xl cursor-pointer">Product</div>
+      </Link>
     </header>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,9 @@ module.exports = {
         '5/7': '71.4285714%',
         '6/7': '85.7142857%',
       },
+      backgroundImage: {
+        'product': "url('/public/image/product-page.png')",
+      },
     },
   },
   variants: {
