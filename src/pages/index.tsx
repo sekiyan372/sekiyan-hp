59a26b9f60cc3a63616e28c4a56b77dd8a416f79
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2021-11-23 04:59:16 +0900

    細かい変更を修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,11 +27,16 @@ import { emailjsConfig } from '~/utils/Emailjs'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGuitar, faBuilding, faSchool } from '@fortawesome/free-solid-svg-icons'
 
-const ICON = require('../../public/my-icon.jpg') 
+// アイコン
 const TWITTER_ICON = require('../../public/Twitter-social-icons-circle-blue.svg')
 const HATENA_ICON = require('../../public/hatenablog-logo.svg')
 const GITHUB_ICON = require('../../public/GitHub-Mark-Light-64px.png')
+
+// ページ内画像
+const ICON = require('../../public/my-icon.jpg')
 const PRODUCT = require('../../public/image/product-page.png')
+
+// ギャラリー画像
 const STRATOCASTER = require('../../public/image/stratocaster.JPG')
 const BASS = require('../../public/image/bass.JPG')
 const LESPAOL = require('../../public/image/lespaol.JPG')
@@ -220,7 +225,7 @@ const Home: NextPage = () => {
           <div>
             <div className="text-white">
               <h3 className="border-b-2">Favorite</h3>
-              <div className="flex">
+              <div className="flex justify-center">
                 <dl className="my-6 mx-10 text-lg">
                   <dt className="float-left clear-left w-20">Instrument</dt>
                   <dd className="float-left ml-5">ベース</dd>
