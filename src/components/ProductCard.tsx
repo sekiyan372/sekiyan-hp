a69db9fdccd55c6c65c780ea8a2048b19084dc2a
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2021-11-24 06:56:06 +0900

    productページを切り出し

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,9 +5,9 @@ type Props = {
   url: string
   imagePath: string
   alt: string
-  title: string
+  title?: string
+  organization?: string
   desc: string
-  role?: string
 }
 
 export const ProductCard: VFC<Props> = ({
@@ -15,18 +15,18 @@ export const ProductCard: VFC<Props> = ({
   imagePath,
   alt,
   title,
+  organization,
   desc,
-  role,
 }) => {
   return(
     <div className="w-104 m-3 border-4 rounded-lg p-3 hover:opacity-80">
       <a href={url} target="blank">
         <Image src={imagePath} alt={alt} />
         <div className="text-center text-2xl">{title}</div>
-        <div className="text-center mt-3">{desc}</div>
-        {role && (
-          <div className="text-center">({role})</div>
+        {organization && (
+          <div className="text-center">{organization}</div>
         )}
+        <div className="text-center mt-3">{desc}</div>
       </a>
     </div>
   )
@@ -37,18 +37,18 @@ export const BigProductCard: VFC<Props> = ({
   imagePath,
   alt,
   title,
+  organization,
   desc,
-  role,
 }) => {
   return(
     <div className="w-160 m-3 border-4 rounded-lg p-3 hover:opacity-80">
       <a href={url} target="blank">
         <Image src={imagePath} alt={alt} />
         <div className="text-center text-2xl">{title}</div>
-        <div className="text-center mt-3">{desc}</div>
-        {role && (
-          <div className="text-center">({role})</div>
+        {organization && (
+          <div className="text-center">({organization})</div>
         )}
+        <div className="text-center mt-3">{desc}</div>
       </a>
     </div>
   )
--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -13,6 +13,66 @@ const OLYMPIC_APP = require('../../public/image/olympic-app.png')
 const CIRKIT_HP = require('../../public/image/cirkit-hp.png')
 const TOTTEKU = require('../../public/image/totteku.png')
 
+const individualProducts = [
+  [
+    {
+      url: '',
+      imagePath: SEKIYAN_HP,
+      alt: "Sekiyan's website",
+      title: 'ポートフォリオサイト',
+      desc: "自分のポートフォリオサイト",
+    },
+    {
+      url: 'https://effector-geek.vercel.app/',
+      imagePath: EFFECTOR_GEEK,
+      alt: "Effector Geek",
+      title: 'Effector Geek',
+      desc: "エフェクター情報共有サイト",
+    },
+  ],
+  [
+    {
+      url: 'https://sekiyan372-calculator.netlify.app/',
+      imagePath: CALCULATOR,
+      alt: "calculator",
+      title: 'calculator',
+      desc: "単純な電卓",
+    },
+    {
+      url: "https://random-app.vercel.app/",
+      imagePath: RANDOM_APP,
+      alt: "Random App",
+      title: "ランダムアプリ",
+      desc: "ランダムに何かを決めるサイト",
+    },
+    {
+      url: "https://saikyo-japan.netlify.app/",
+      imagePath: OLYMPIC_APP,
+      alt: "Olympic App",
+      title: "ぼくのかんがえたさいきょうのさむらいじゃぱん",
+      desc: "個人の考えた侍ジャパンを作って遊ぶサイト",
+    },
+  ],
+]
+
+const joinProduct = [
+  {
+    url: "https://www.cirkit.jp/",
+    imagePath: CIRKIT_HP,
+    alt: "CirKit HP",
+    organization: "株式会社CirKit",
+    desc: "学生ベンチャー企業のコーポレートサイト",
+  },
+  {
+    url: "https://totteku.netlify.app/",
+    imagePath: TOTTEKU,
+    alt: "TOTTEKU",
+    title: "撮っテク！",
+    organization: "Tourism プロジェクト",
+    desc: "湯涌町でのイベントのためのサイト",
+  },
+]
+
 const Product: NextPage = () => {
   return(
     <>
@@ -24,66 +84,49 @@ const Product: NextPage = () => {
         <div className="p-10">
           <div className="mb-20">
             <SubHeading>Individual</SubHeading>
-            <div className="flex flex-wrap justify-center">
-              <BigProductCard
-                url=""
-                imagePath={SEKIYAN_HP}
-                alt="Sekiyan's website"
-                title="ポートフォリオサイト"
-                desc="このサイト"
-              />
-              <BigProductCard
-                url="https://effector-geek.vercel.app/"
-                imagePath={EFFECTOR_GEEK}
-                alt="Effector Geek"
-                title="Effector Geek"
-                desc="エフェクター情報共有サイト"
-              />
-            </div>
-            <div className="flex flex-wrap justify-center">
-              <ProductCard
-                url="https://sekiyan372-calculator.netlify.app/"
-                imagePath={CALCULATOR}
-                alt="calculator"
-                title="calculator"
-                desc="単純な電卓"
-              />
-              <ProductCard
-                url="https://random-app.vercel.app/"
-                imagePath={RANDOM_APP}
-                alt="Random App"
-                title="ランダムアプリ"
-                desc="ランダムに何かを決めるサイト"
-              />
-              <ProductCard
-                url="https://saikyo-japan.netlify.app/"
-                imagePath={OLYMPIC_APP}
-                alt="Olympic App"
-                title="ぼくのかんがえたさいきょうのさむらいじゃぱん"
-                desc="個人の考えた侍ジャパンを作って遊ぶサイト"
-              />
-            </div>
+            {individualProducts.map((productList, index) => (
+              <div className="flex flex-wrap justify-center" key={index}>
+                {productList.map(product => (
+                  <span key={product.title}>
+                    {index === 0 && (
+                      <BigProductCard
+                        url={product.url}
+                        imagePath={product.imagePath}
+                        alt={product.alt}
+                        title={product.title}
+                        desc={product.desc}
+                      />
+                    )}
+                    {index === 1 && (
+                      <ProductCard
+                      url={product.url}
+                      imagePath={product.imagePath}
+                      alt={product.alt}
+                      title={product.title}
+                      desc={product.desc}
+                    />
+                    )}
+                  </span>
+                ))}
+              </div>
+            ))}
           </div>
 
           <div>
             <SubHeading>Join</SubHeading>
             <div className="flex flex-wrap justify-center">
-              <ProductCard
-                url="https://www.cirkit.jp/"
-                imagePath={CIRKIT_HP}
-                alt="CirKit HP"
-                title="株式会社CirKit"
-                desc="学生ベンチャー企業のコーポレートサイト"
-                role="企画から制作リーダーまでを担当"
-              />
-              <ProductCard
-                url="https://totteku.netlify.app/"
-                imagePath={TOTTEKU}
-                alt="TOTTEKU"
-                title="撮っテク！"
-                desc="湯涌町でのイベントのためのサイト"
-                role="運営メンバーとして制作を担当"
-              />
+              {joinProduct.map(product => (
+                <span key={product.title}>
+                  <ProductCard
+                    url={product.url}
+                    imagePath={product.imagePath}
+                    alt={product.alt}
+                    title={product.title}
+                    organization={product.organization}
+                    desc={product.desc}
+                  />
+                </span>
+              ))}
             </div>
           </div>
         </div>
