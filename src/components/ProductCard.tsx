0a07be35355464e9da6a2a7bfe47e3a55f02bed9
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2021-11-26 13:49:09 +0900

    技術ラベルを作成

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { VFC } from 'react'
 import Image from 'next/image'
+import TechLavel from '~/components/TechLavel'
 
 type Props = {
   url?: string
@@ -8,6 +9,7 @@ type Props = {
   title?: string
   organization?: string
   desc: string
+  technologies?: string[]
 }
 
 export const ProductCard: VFC<Props> = ({
@@ -17,6 +19,7 @@ export const ProductCard: VFC<Props> = ({
   title,
   organization,
   desc,
+  technologies,
 }) => {
   return(
     <div className="md:w-104 m-3 border-4 rounded-lg p-3 hover:opacity-80 text-center">
@@ -27,6 +30,9 @@ export const ProductCard: VFC<Props> = ({
           <div>{organization}</div>
         )}
         <div className="mt-3">{desc}</div>
+        {technologies && technologies.map(tech => (
+          <TechLavel key={tech}>{tech}</TechLavel>
+        ))}
       </a>
     </div>
   )
@@ -39,16 +45,20 @@ export const BigProductCard: VFC<Props> = ({
   title,
   organization,
   desc,
+  technologies,
 }) => {
   return(
-    <div className="md:w-160 m-3 border-4 rounded-lg p-3 hover:opacity-80">
+    <div className="md:w-160 m-3 border-4 rounded-lg p-3 hover:opacity-80 text-center">
       <a href={url} target="blank">
         <Image src={imagePath} alt={alt} />
-        <div className="text-center text-2xl">{title}</div>
+        <div className="text-2xl">{title}</div>
         {organization && (
-          <div className="text-center">({organization})</div>
+          <div>({organization})</div>
         )}
-        <div className="text-center mt-3">{desc}</div>
+        <div className="my-3">{desc}</div>
+        {technologies && technologies.map(tech => (
+          <TechLavel key={tech}>{tech}</TechLavel>
+        ))}
       </a>
     </div>
   )
--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -22,6 +22,7 @@ const individualProducts = [
       alt: "Sekiyan's website",
       title: 'ポートフォリオサイト',
       desc: "自分のポートフォリオサイト",
+      tech: ['TypeScript', 'React', 'Next.js', 'Tailwind CSS', 'Vercel'],
     },
     {
       url: 'https://effector-geek.vercel.app/',
@@ -29,6 +30,7 @@ const individualProducts = [
       alt: "Effector Geek",
       title: 'Effector Geek',
       desc: "エフェクター情報共有サイト",
+      tech: ['TypeScript', 'React', 'Redux', 'Next.js', 'Firebase', 'Tailwind CSS', 'Vercel'],
     },
   ],
   [
@@ -38,6 +40,7 @@ const individualProducts = [
       alt: "calculator",
       title: 'calculator',
       desc: "単純な電卓",
+      tech: ['TypeScript', 'React', 'Styled Components', 'Netrify'],
     },
     {
       url: "https://random-app.vercel.app/",
@@ -45,6 +48,7 @@ const individualProducts = [
       alt: "Random App",
       title: "ランダムアプリ",
       desc: "ランダムに何かを決めるサイト",
+      tech: ['TypeScript', 'React', 'Styled Components', 'Vercel'],
     },
     {
       url: "https://saikyo-japan.netlify.app/",
@@ -52,6 +56,7 @@ const individualProducts = [
       alt: "Olympic App",
       title: "ぼくのかんがえたさいきょうのさむらいじゃぱん",
       desc: "個人の考えた侍ジャパンを作って遊ぶサイト",
+      tech: ['JavaScript', 'Vue', 'Nuxt.js', 'Netrify'],
     },
   ],
 ]
@@ -64,6 +69,7 @@ const joinProduct = [
     title: "CirKitホームページ",
     organization: "株式会社CirKit",
     desc: "学生ベンチャー企業のコーポレートサイト",
+    tech: ['TypeScript', 'React'],
   },
   {
     imagePath: MAIL,
@@ -71,6 +77,7 @@ const joinProduct = [
     title: "@連絡網",
     organization: "株式会社CirKit",
     desc: "受託開発のメーリングサービス",
+    tech: ['Ruby on Rails', 'JavaScript', 'Sass', 'Bulma', 'EC2', 'S3']
   },
   {
     url: "https://totteku.netlify.app/",
@@ -79,6 +86,7 @@ const joinProduct = [
     title: "撮っテク！",
     organization: "Tourism プロジェクト",
     desc: "湯涌町でのイベントのためのサイト",
+    tech: [' TypeScript', 'Vue', 'Nuxt.js', 'Netrify'],
   },
 ]
 
@@ -104,6 +112,7 @@ const Product: NextPage = () => {
                         alt={product.alt}
                         title={product.title}
                         desc={product.desc}
+                        technologies={product.tech}
                       />
                     )}
                     {index === 1 && (
@@ -113,6 +122,7 @@ const Product: NextPage = () => {
                       alt={product.alt}
                       title={product.title}
                       desc={product.desc}
+                      technologies={product.tech}
                     />
                     )}
                   </span>
@@ -133,6 +143,7 @@ const Product: NextPage = () => {
                     title={product.title}
                     organization={product.organization}
                     desc={product.desc}
+                    technologies={product.tech}
                   />
                 </span>
               ))}
