fd423a8e4810ea13b1b32332bdd3e26303d790fb
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2022-11-03 23:22:53 +0900

    feat: add link and change icon

--- src/components/Button/LinkIcon.tsx
+++ src/components/Button/LinkIcon.tsx
@@ -1,39 +1,25 @@
 import ClassNames from 'classnames'
-import Image from 'next/image'
 import Link from 'next/link'
 import type { FC, HTMLAttributes } from 'react'
 
-type DivProps = HTMLAttributes<HTMLDivElement>
-
 type Props = {
   url: string
-  src: string
-  alt: string
-  width: number
-  height: number
-} & DivProps
+  children: React.ReactNode
+} & HTMLAttributes<HTMLDivElement>
 
-export const LinkIcon: FC<Props> = ({
-  url,
-  src,
-  alt,
-  width,
-  height,
-  className,
-}) => {
+export const LinkIcon: FC<Props> = ({ url, children, className }) => {
   return (
-    <div className={ClassNames('inline-block md:mx-5 mx-3', className)}>
-      <Link href={url} passHref>
-        <a target="_blank">
-          <Image
-            src={src}
-            alt={alt}
-            width={width}
-            height={height}
-            className="cursor-pointer hover:opacity-50"
-          />
-        </a>
-      </Link>
-    </div>
+    <Link href={url} passHref>
+      <a target="_blank">
+        <div
+          className={ClassNames(
+            'inline-block md:mx-5 mx-3 p-3 bg-stone-200 rounded-full',
+            className
+          )}
+        >
+          {children}
+        </div>
+      </a>
+    </Link>
   )
 }
