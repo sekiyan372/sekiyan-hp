a4f16c723db99d3f454f942d1886faf05f645d8f
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2022-11-14 18:34:01 +0900

    feat: members table

--- src/pages/ohisama.tsx
+++ src/pages/ohisama.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from 'next'
 import Image from 'next/image'
 
 import { SubscribedTalk } from '~/components/Label'
+import { Footer, Head, Header } from '~/components/Layout'
 import { Heading } from '~/components/Text'
 
 const talks: string[] = ['#naotalk', '#hinatalk', '#mikutalk']
@@ -10,6 +11,10 @@ const talks: string[] = ['#naotalk', '#hinatalk', '#mikutalk']
 const OhisamaPage: NextPage = () => {
   return (
     <>
+      <Head title="おひさまhistory" />
+
+      <Header />
+
       <section className="text-gray-800">
         <Heading className="text-4xl">おひさまProfile</Heading>
         <div className="flex justify-center">
@@ -42,15 +47,34 @@ const OhisamaPage: NextPage = () => {
             <div className="py-1">プレイ中：ひなこい</div>
           </div>
         </div>
+
+        <table className="text-left text-2xl mx-auto my-10">
+          <tbody>
+            <tr className="border-y-2">
+              <th className="w-32 p-2">最推し</th>
+              <th className="p-2">小坂 菜緒</th>
+            </tr>
+            <tr className="border-y-2">
+              <th className="w-32 p-2">推し</th>
+              <th className="p-2">河田 陽菜・金村 美玖・森本 茉莉</th>
+            </tr>
+            <tr className="border-y-2">
+              <th className="w-32 p-2">気になる</th>
+              <th className="p-2">岸 帆夏・竹内 希来里・渡辺 莉奈</th>
+            </tr>
+          </tbody>
+        </table>
       </section>
 
-      <section className="bg-hinata text-gray-800">
+      <section className="bg-hinata text-white">
         <Heading className="text-4xl">ライブ参戦履歴</Heading>
       </section>
 
       <section className="text-gray-800">
         <Heading className="text-4xl">セルフQ&A</Heading>
       </section>
+
+      <Footer />
     </>
   )
 }
